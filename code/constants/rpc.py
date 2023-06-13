01e3fa956508c9dc2da208930fe55365df1b6e13
RealPlurr <93705241+RealPlurr@users.noreply.github.com>
2023-06-13 15:34:36 +0300

    Add files via upload

---
+++ code/constants/rpc.py
@@ -0,0 +1,10 @@
+# Discord Rich Presence Settings
+from __future__ import annotations
+
+ENABLED = False
+CLIENT_ID = "1118132145874026537"
+LARGE_IMAGE = "launcher_logo"
+CONFIG_IMAGE = "config"
+ROOT_IMAGE = "main"
+VANILLA_IMAGE = "vanilla"
+MODDED_IMAGE = "modded"
